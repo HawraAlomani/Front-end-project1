6f361facb55cd57f678b91e1b15b59c724494e67
Hawra Alomani <ha0073@mix.wvu.edu>
2022-06-25 22:41:36 +0300

    copying function js is done

--- js/copyQuotes.js
+++ js/copyQuotes.js
@@ -1,25 +1,11 @@
-
-// let information = document.getElementsByClassName("alert-copy");
-// for (let i = 0; i < information.length; i++) {
-//   information[i].style.color = '#46413b';
-// }
-
-
-// function to copy quotes
-//id click1 <a>
-//id quote1 <p>
-function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("myInput");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-     /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-  
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+// function to copy quotes by passing the id of p
+function copyFunction(id) {
+  const copyText = document.getElementById(id).textContent;
+  const textArea = document.createElement("textarea");
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  textArea.remove();
+  alert("The quote is copied!");
 }
-
