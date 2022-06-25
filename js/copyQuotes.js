5acbcbb818a46f53f608150a50ac35319db51015
Hawra Alomani <ha0073@mix.wvu.edu>
2022-06-25 17:54:19 +0300

    copy Quote issue Not working in arabic-q.html page

--- js/copyQuotes.js
+++ js/copyQuotes.js
@@ -1,5 +1,25 @@
 
-let information = document.getElementsByClassName("alert-copy");
-for (let i = 0; i < information.length; i++) {
-  information[i].style.color = '#46413b';
+// let information = document.getElementsByClassName("alert-copy");
+// for (let i = 0; i < information.length; i++) {
+//   information[i].style.color = '#46413b';
+// }
+
+
+// function to copy quotes
+//id click1 <a>
+//id quote1 <p>
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
 }
+
